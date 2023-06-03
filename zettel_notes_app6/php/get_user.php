361b96b54f5b45cb77c9990fcbb0effa3e72949b
LYuree <slymerrow@gmail.com>
2023-06-03 12:02:19 +0700

    Keywords are loaded automatically from the DB when the user signs in.

---
+++ zettel_notes_app6/php/get_user.php
@@ -0,0 +1,51 @@
+<?php
+require "./db_con.php";
+require "./functions.php";
+if(!empty($_POST) && isset($_POST['username'])){
+    try{
+        $username = $_POST['username'];
+        //AUTHORIZATION
+        try{
+            $username = $_POST['username'];
+            $results = $pdo->query("SELECT name, password, email FROM users
+            WHERE name = '$username'
+            OR
+            email = '$username'"); 
+        }catch (PDOException $e) {
+            $results = null;
+            $pdo = null;
+            exit;
+        }
+
+        try{
+            $row = $results -> fetch(PDO::FETCH_NUM);
+            if(count($row) == 0 || !is_countable($row)){
+                $results = null;
+                echo json_encode($results);
+                exit;
+            }
+            else{
+                // $row = $results -> fetch(PDO::FETCH_NUM);
+                $db_password = $row[1];
+                if(password_verify($_POST['password'], $db_password))
+                $userEntry = new stdClass();
+                $userEntry->active = "true";
+                echo json_encode($userEntry);
+                exit;
+            }
+        }catch (PDOException $e) {
+            $results = null;
+            $pdo = null;
+            exit;
+        }
+    }catch(PDOException $e){
+        $pdo = null;
+        echo '' . $e->getMessage(); 
+        exit;
+    }
+}
+
+
+
+
+?>
