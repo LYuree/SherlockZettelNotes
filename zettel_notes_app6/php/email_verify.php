467603469e011e10c8604b85e0c75eb23994d0aa
LYuree <slymerrow@gmail.com>
2023-06-09 11:22:05 +0700

    Complete implementation of the registration (sign up) process. Email verification is added to the DB and deleted when the user follows the link from an verification email. Their user entry then becomes active and they sign in to create their notes and work with them.

--- zettel_notes_app6/php/email_verify.php
+++ zettel_notes_app6/php/email_verify.php
@@ -4,13 +4,65 @@ require "./functions.php";
 
 if(!empty($_GET)){
     if(isset($_GET['verification_code'])){
-        $verificationToken = $_GET['verification_code'];
-        $results = $pdo->query("SELECT * FROM user_tokens join users
-        on user_id = users.id
-        WHERE token = '$verificationToken'");
-
+        try{
+            $verificationToken = $_GET['verification_code'];
+            $results = $pdo->query("SELECT * FROM user_tokens join users
+            on user_id = users.id
+            WHERE token = '$verificationToken'");
+            $row = $results -> fetch(PDO::FETCH_ASSOC);
+            $username = $row['name'];
+            $update_query_string = "UPDATE users SET active = true
+                            WHERE name = '$username'";
+            $pdo->query($update_query_string);
+            $pdo->query("DELETE from user_tokens where token = '$verificationToken'");
+            $verification_message = "E-mail успешно подтверждён!";
+            $main_page_link_display = "block";
+        }
+        catch (PDOException $e) {
+            $verification_message = "Что-то пошло не так...";
+            $main_page_link_display = "none";
+            $results = null;
+            $pdo = null;
+            exit;
+        }
     }
 }
 
-
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p class="verification_message"><?=$verification_message?></p>
+    
+    <div class="modal__sign-in__button-wrapper__flex-container">
+        <div class="modal__sign-in__button-wrapper">
+            <button type="submit" class="main_button">Войти</button>
+        </div>
+    </div>
+
+    <style>
+        p.verification_message,
+        button.main_button
+        {
+            text-align: center;
+            /* vertical-align: middle; */
+        }
+        p.verification_message {
+            position: relative;
+            top: -50%;
+            /* left: 50%; */
+            /* transform: translate(-50%, -50%);    */
+        }
+        button.main_button {
+            display: <?=$main_page_link_display?>;
+        }
+    </style>
+</body>
+</html>
--- zettel_notes_app6/php/functions.php
+++ zettel_notes_app6/php/functions.php
@@ -1,4 +1,14 @@
-<?php 
+<?php
+
+    use PHPMailer\PHPMailer\PHPMailer;
+    use PHPMailer\PHPMailer\Exception;
+
+    require 'PHPMailer/src/Exception.php';
+    require 'PHPMailer/src/PHPMailer.php';
+    require 'PHPMailer/src/SMTP.php';    
+
+    // php\sensitive_info\sensitive_info.php
+    // echo "\nfunctions: ".$noreply_email." ".$noreply_app_code;
 
     function getNotes(){
         $resultArray = array();
@@ -23,6 +33,83 @@
     function deleteNote(){
 
     }
+
+// function send_verification_link($activation_code)
+function send_verification_link($email, $activation_code, $sender_email, $sender_app_code){
+    // echo "send_link: ".$noreply_email." ".$noreply_app_code;
+
+    $expFormat = mktime(
+    date("H"), date("i"), date("s"), date("m") ,date("d")+1, date("Y")
+    );
+    $expDate = date("Y-m-d H:i:s",$expFormat);
+    // $key = md5(strval(2418*2).$email);
+    // $addKey = substr(md5(uniqid(rand(),1)),3,10);
+    // $key = $key . $addKey;
+    $key = $activation_code;
+    // Insert Temp Table
+    // $pdo->query("INSERT INTO `password_reset_tokens` (`email`, `key`, `expDate`) VALUES ('".$email."', '".$key."', '".$expDate."');");
+    // echo "email_sender, key = ".$key;
+    $output='<p>Dear user,</p>';
+    $output.='<p>Please click on the following link to reset your password.</p>';
+    $output.='<p>-------------------------------------------------------------</p>';
+    // $output.='<p><a href="https://www.allphptricks.com/forgot-password/reset-password.php?
+    // key='.$key.'&email='.$email.'&action=reset" target="_blank">
+    // https://www.allphptricks.com/forgot-password/reset-password.php
+    // ?key='.$key.'&email='.$email.'&action=reset</a></p>';	
+    $output.='<p><a href="http://localhost:3000/PHP/zettel_notes_app6/php/email_verify.php?verification_code='.$key;
+    $output.='&action="reset" target="_blank">sherlocknotes.php</a></p>';
+
+    $output.='<p>-------------------------------------------------------------</p>';
+    $output.='<p>Please be sure to copy the entire link into your browser.
+    The link will expire after 1 day for security reasons.</p>';
+    $output.='<p>If you did not request this forgotten password email, no action 
+    is needed, your password will not be reset. However, you may want to log into 
+    your account and change your security password as someone may have guessed it.</p>';   	
+    $output.='<p>Thanks,</p>';
+    $output.='<p>Sherlock Notes</p>';
+    $body = $output; 
+    $subject = "Sherlock Notes - подтверждение почтового адреса";
+
+    $email_to = $email;
+   //  $fromserver = "index.php";
+    $fromserver = "noreplylibrarium@gmail.com";
+   //  require("PHPMailer/PHPMailerAutoload.php");
+    $mail = new PHPMailer(true);
+    $mail->IsSMTP();
+    // $mail->Host = "mail.yourwebsite.com"; // Enter your host here
+    $mail->Host = "smtp.gmail.com"; 
+    $mail->SMTPAuth = true;
+    $mail->Username = $sender_email; // Enter your email here
+    $mail->Password = $sender_app_code; //Enter your password here
+    $mail->Port = 465;
+
+    //****************************
+    $mail->SMTPSecure = "ssl";
+    //****************************
+    
+    $mail->IsHTML(true); //for the letter itself?
+
+   //  $mail->From = "noreply@yourwebsite.com";
+   
+   //  $mail->From = "lereanon@mail.ru";
+    $mail->setFrom("noreplylibrarium@gmail.com");
+    $mail->addAddress($email);
+   //  $mail->Subject = "Восстановление пароля: код подтверждения";
+   
+    $mail->FromName = "Sherlock Notes";
+
+    $mail->Sender = $fromserver; // indicates ReturnPath header
+    $mail->Subject = $subject;
+    $mail->Body = $body;
+    $mail->AddAddress($email_to);
+    if(!$mail->Send()){
+    echo "Mailer Error: " . $mail->ErrorInfo;
+    }else{
+    echo "<div class='error'>
+    <p>An email has been sent to you with instructions on how to reset your password.</p>
+    </div><br /><br /><br />";
+    }    
+}
 
 
 
