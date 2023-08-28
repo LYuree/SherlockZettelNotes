0aa78c0dd628dcbd747cd46f25971b4c2eb09f66
LYuree <slymerrow@gmail.com>
2023-08-28 13:21:32 +0700

    Users now can set the publicity (accessibility) parameter for their accounts. This will determine whether their keywords information is accessible to other users and, thus, whether they can be recommended as a cowork candidate to another client based on the matches in their keyword sets. The stylingon some fields and buttons got a little bugged, gotta fix that.

--- zettel_notes_app6/php/get_keywords_by_users.php
+++ zettel_notes_app6/php/get_keywords_by_users.php
@@ -4,25 +4,26 @@ require "./functions.php";
 // echo "GET YOUR ASS DOWN TO ME!";
 // echo "". empty($_GET);
 if(isset($_GET)){
-    $query_string = "SELECT name from users where active is true";
+    $query_string = "SELECT name, email from users where active is true AND public is true";
     $results = $pdo->query($query_string);
-    $usernames = array();
-    while($name = $results->fetch(PDO::FETCH_ASSOC)){
-        $usernames[] = $name['name'];
+    $users = array();
+    while($user = $results->fetch(PDO::FETCH_ASSOC)){
+        $users[] = $user;
     }
     // creating an array of objects storing the nickname of a user
     // and an array of his keywords, their frequencies and ranks
     $usersAndKeywords = array();
-    foreach($usernames as $name){
+    foreach($users as $user){
         // $query_string = "SELECT keyword, occurences
         //     from users join keywords
         //     on id = userid
         //     where name = '$name'";
         // $results = $pdo->query($query_string);
         // echo "$name";
-        $rankedKeywords = getRankedKeywords($pdo, $name);
+        $rankedKeywords = getRankedKeywords($pdo, $user['name']);
         $item = new stdClass();
-        $item->username = $name;
+        $item->username = $user['name'];
+        $item->email = $user['email'];
         $item->keywords = $rankedKeywords;
         $usersAndKeywords[] = $item;
         // while($row = $results->fetch(PDO::FETCH_ASSOC)){
